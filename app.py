af0d5793308354b6490ccaeb438bca11508dbc44
Vasudev Kartheek Akkur <43079315+kartheekakkur@users.noreply.github.com>
2020-03-29 20:49:14 -0500

    include circleci

--- app.py
+++ app.py
@@ -55,16 +55,16 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    LOG.info("JSON payload: {}", json_payload)
+    LOG.info("JSON payload: %p", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("Inference payload DataFrame: {}", inference_payload)
+    LOG.info("Inference payload DataFrame: %i", inference_payload)
 
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info("prediction: {}" , prediction)
+    LOG.info("prediction: %s" , prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
